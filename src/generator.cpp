709277fb03a723c0674f61ac60448d592234089e
sprinkuls <maymacler@gmail.com>
2026-03-02 22:20:23 -0500

    add test generator and sample tests

---
+++ src/generator.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cout << "usage:\n";
+        cout << argv[0] << " <k> <m>\n";
+        cout << "where 'k' is the cache size,\n";
+        cout << "and   'm' is the number of requests to make\n";
+        return 1;
+    }
+
+    int k = stoi(argv[1]);
+    int m = stoi(argv[2]);
+
+    // randomization boilerplate
+    random_device rd;
+    mt19937 g(rd());
+    uniform_int_distribution<int> int_dist(0, m / 2);
+
+    vector<int> generated;
+    for (int i = 0; i < m; i++) {
+        int smallest = int_dist(g);
+        for (int j = 1; j < 2; j++) {
+            smallest = min(smallest, int_dist(g));
+        }
+        generated.push_back(smallest);
+    }
+
+    cout << k << " " << m << "\n";
+    for (int request : generated) {
+        cout << request << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
