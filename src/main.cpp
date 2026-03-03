275a23ebfddcdbd71cb14c54aea6b64fba56854a
sprinkuls <maymacler@gmail.com>
2026-03-02 20:29:34 -0500

    implement LRU policy

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <queue>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
-
-int fifo(int capacity, int num_requests, vector<int>& pages) {
+int fifo(int capacity, int num_requests, vector<int> &pages) {
     unordered_set<int> s; // stores current pages in memory
-    queue<int> indexes; // stores order of pages for FIFO
+    queue<int> indexes;   // stores order of pages for FIFO
     int page_faults = 0;
 
     for (int i = 0; i < num_requests; i++) {
@@ -30,11 +30,50 @@ int fifo(int capacity, int num_requests, vector<int>& pages) {
         }
     }
 
-    return page_faults;    
-    
+    return page_faults;
 }
 
-int main(int argc, char* argv[]) {
+int lru(int capacity, int num_requests, vector<int> &requests) {
+    int misses = 0;
+
+    vector<int> values;
+    vector<int> access_times;
+
+    for (int i = 0; i < num_requests; i++) {
+        int request = requests[i];
+        auto iter = find(values.begin(), values.end(), request);
+
+        if (iter == values.end()) {
+            // not in cache
+            if (values.size() < capacity) {
+                // cache not full
+                values.push_back(request);
+                access_times.push_back(i);
+            } else {
+                // cache full, kick something out
+                int oldest_seen = i + 1; // dummy value greater than current time
+                int oldest_index = -1;
+                for (int j = 0; j < capacity; j++) {
+                    if (access_times[j] < oldest_seen) {
+                        oldest_seen = access_times[j];
+                        oldest_index = j;
+                    }
+                }
+                values[oldest_index] = request;
+                access_times[oldest_index] = i;
+            }
+            misses++;
+        } else {
+            // value in cache, need to update access time
+            int index = iter - values.begin();
+            access_times[index] = i;
+        }
+    }
+
+    return misses;
+}
+
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         cerr << "Usage: ./test <input_file>" << endl;
         return 1;
@@ -42,7 +81,8 @@ int main(int argc, char* argv[]) {
 
     ifstream infile(argv[1]);
     int k, m;
-    if (!(infile >> k >> m)) return 0;
+    if (!(infile >> k >> m))
+        return 0;
 
     vector<int> requests(m);
     for (int i = 0; i < m; i++) {
@@ -50,6 +90,7 @@ int main(int argc, char* argv[]) {
     }
 
     cout << "FIFO: " << fifo(k, m, requests) << endl;
+    cout << "LRU: " << lru(k, m, requests) << "\n";
 
     return 0;
 }
